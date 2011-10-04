a11fe441718382829183aeb9c959351ae38364ac
Dorian Raymer <deldotdr@gmail.com>
2011-10-03 18:34:52 -0700

    Disable bootstrap logging config; now in Runner

--- pyon/core/bootstrap.py
+++ pyon/core/bootstrap.py
@@ -6,21 +6,21 @@ __license__ = 'Apache 2.0'
 from pyon.util.config import Config
 from pyon.core.object import IonObjectRegistry
 
-import logging.config
-import os
+#import logging.config
+#import os
 
 # Note: do we really want to do the res folder like this again?
-logging_conf_paths = ['res/config/logging.yml', 'res/config/logging.local.yml']
-LOGGING_CFG = Config(logging_conf_paths).data
+#logging_conf_paths = ['res/config/logging.yml', 'res/config/logging.local.yml']
+#LOGGING_CFG = Config(logging_conf_paths).data
 
 # Ensure the logging directories exist
-for handler in LOGGING_CFG.get('handlers', {}).itervalues():
-    if 'filename' in handler:
-        log_dir = os.path.dirname(handler['filename'])
-        if not os.path.exists(log_dir):
-            os.makedirs(log_dir)
+#for handler in LOGGING_CFG.get('handlers', {}).itervalues():
+#    if 'filename' in handler:
+#        log_dir = os.path.dirname(handler['filename'])
+#        if not os.path.exists(log_dir):
+#            os.makedirs(log_dir)
 
-logging.config.dictConfig(LOGGING_CFG)
+#logging.config.dictConfig(LOGGING_CFG)
 
 conf_paths = ['res/config/pyon.yml', 'res/config/pyon.local.yml']
 CFG = Config(conf_paths).data
